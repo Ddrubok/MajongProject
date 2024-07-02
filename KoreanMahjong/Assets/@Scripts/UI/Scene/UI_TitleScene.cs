0ce56872fa0a93d0a0e4c24a4012222b67a4ca11
Ddurubok <yadomii@naver.com>
2024-07-02 17:43:37 +0900

    -doobok- 다른 프로젝트 추가

--- KoreanMahjong/Assets/@Scripts/UI/Scene/UI_TitleScene.cs
+++ KoreanMahjong/Assets/@Scripts/UI/Scene/UI_TitleScene.cs
@@ -51,8 +51,6 @@ public class UI_TitleScene : UI_Scene
 
 				GetObject((int)GameObjects.StartImage).gameObject.SetActive(true);
 				GetText((int)Texts.DisplayText).text = "Touch To Start";
-
-
 			}
 		});
 	}
